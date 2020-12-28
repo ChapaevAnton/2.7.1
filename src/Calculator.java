c8b023c2ec308f3bda06ca59560eaf275fd5caad
ChapaevAnton <virsus2009@gmail.com>
2020-12-28 14:08:14 +0700

    2.7.2 fix bag

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Calculator {
 
     private Scanner enter = new Scanner(System.in);
+    private Operand opr1, opr2;
+    private Operator operation;
 
     //TODO 2.7.2*
     public void calculate() {
-        Operand opr1, opr2;
-        Operator operation;
+
         float result;
         System.out.println("калькулятор\ns - выход, c - сброс");
         opr1 = new Operand(getNumber());
@@ -44,19 +45,23 @@ public class Calculator {
     private float getCalculate(Operand opr1, Operand opr2, Operator operation) {
         float result;
         switch (operation.getOperator()) {
+
             case '+': {
                 result = opr1.getOperand() + opr2.getOperand();
                 break;
             }
+
             case '-': {
                 result = opr1.getOperand() - opr2.getOperand();
                 break;
             }
+
             case '*': {
                 result = opr1.getOperand() * opr2.getOperand();
 
                 break;
             }
+
             case '/': {
                 if (opr2.getOperand() != 0 && operation.getOperator() == '/') {
                     result = opr1.getOperand() / opr2.getOperand();
@@ -67,12 +72,13 @@ public class Calculator {
                 }
                 break;
             }
+
             case 'c': {
                 System.out.println("Сброс!");
-                result = getCalculate(
-                        new Operand(getNumber()),
-                        new Operand(getNumber()),
-                        new Operator(getOperation()));
+                this.opr1 = new Operand(getNumber());
+                this.opr2 = new Operand(getNumber());
+                this.operation = new Operator(getOperation());
+                result = getCalculate(this.opr1,this.opr2,this.operation);
                 break;
             }
 
@@ -80,11 +86,13 @@ public class Calculator {
                 result = 0F;
                 break;
             }
+
             default:
                 System.out.println("Вы допустили ошибку при вводе типа операции. Попробуйте еще раз!!!");
                 operation.setOperator(getOperation());
                 result = getCalculate(opr1, opr2, operation);
         }
+
         return result;
     }
 
